e7edbc7f047780703318cf18eb7a2fe2ace20a51
Aulia Wira Yudha Mutopa <yudha8aulia@gmail.com>
2023-11-27 20:48:40 +0700

    update inbox

--- app/src/main/java/com/example/loundryapps/fragment/InboxFragment.kt
+++ app/src/main/java/com/example/loundryapps/fragment/InboxFragment.kt
@@ -1,18 +1,57 @@
 package com.example.loundryapps.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.loundryapps.R
+import com.example.loundryapps.Nav.Navbar
+import com.example.loundryapps.activity.NotifikasiActivity
+import com.example.loundryapps.activity.ulasanActivity
+import com.example.loundryapps.databinding.FragmentInboxBinding
 
 class InboxFragment : Fragment() {
+    private var _binding: FragmentInboxBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inbox, container, false)
+    ): View {
+        _binding = FragmentInboxBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val btnbeck: ImageView = binding.beck
+        btnbeck.setOnClickListener {
+            onBackPressed()
+        }
+
+        val btnHuhu: TextView = binding.ulasan
+        btnHuhu.setOnClickListener {
+            naviToUlas()
+        }
+        val btnnot: TextView = binding.notif
+        btnnot.setOnClickListener {
+            naviToNotif()
+        }
+
+
+        return root
+    }
+
+    private fun onBackPressed() {
+        val intent = Intent(requireActivity(), Navbar::class.java)
+        startActivity(intent)    }
+
+    private fun naviToUlas() {
+        val intent = Intent(requireActivity(), ulasanActivity::class.java)
+        startActivity(intent)
+    }
+    private fun naviToNotif() {
+        val intent = Intent(requireActivity(), NotifikasiActivity::class.java)
+        startActivity(intent)
     }
 }
