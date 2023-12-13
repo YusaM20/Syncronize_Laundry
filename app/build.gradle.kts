cf8242274b816a41e921ab4fda2611700fc2811b
Aulia Wira Yudha Mutopa <yudha8aulia@gmail.com>
2023-12-13 11:18:53 +0700

    login regis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,6 +51,10 @@ dependencies {
     implementation ("com.google.android.gms:play-services-location:21.0.1")
     implementation ("com.google.android.libraries.places:places:2.7.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/loundryapps/Login/Login.kt
+++ app/src/main/java/com/example/loundryapps/Login/Login.kt
@@ -1,14 +1,26 @@
 package com.example.loundryapps.Login
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.loundryapps.Nav.Navbar
 import com.example.loundryapps.R
 import com.example.loundryapps.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class login : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var googleSignInClient : GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +29,58 @@ class login : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.logugle.setOnClickListener { loginwithFbGoogle() }
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
         binding.daftr.setOnClickListener { regis() }
         binding.log.setOnClickListener { logint() }
+        binding.logugle.setOnClickListener {
+            googleSignIn();
+        }
 
 
     }
 
-    private fun loginwithFbGoogle() {
-        val i = Intent(this, Navbar::class.java)
-        startActivity(i)
+    private fun googleSignIn() {
+        val signInClient = googleSignInClient.signInIntent
+        launcher.launch(signInClient)
+
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result->
+        if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            manageresult(task);
+        }
+    }
+
+    private fun manageresult(task: Task<GoogleSignInAccount>) {
+        val account : GoogleSignInAccount? = task.result
+
+        if (account !== null){
+            val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+            firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+                if (task.isSuccessful){
+                    val intent = Intent(this, Navbar::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(this,"Account created",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        else{
+            Toast.makeText(this,task.exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun regis() {
